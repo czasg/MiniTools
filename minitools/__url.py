5630af41cfbd20eff858dfb580fa42d51d96368f
cza <chenziangsg@163.com>
2019-11-04 00:12:17 +0800

    update next_page func

--- minitools/__url.py
+++ minitools/__url.py
@@ -4,9 +4,6 @@ __all__ = 'next_page',
 
 
 def next_page(url, rule, step=1, replace=None):
-    if replace and re.search(replace[0], url):
-        url = url.replace(replace[0], replace[1])
-    next_page = re.sub('\(.*\)', str(
-        int(re.search(rule, url, re.S).group(1)) + step
-    ), rule)
-    return re.sub(rule, next_page, url, re.S)
+    url = url.replace(*replace) if replace and re.search(replace[0], url) else None
+    rule = re.sub('(.*)(\(.*)', '(\\1)\\2', rule) if rule.count('(') == 1 else None
+    return re.sub(rule, lambda x: f'{x.group(1)}{int(x.group(2)) + step}', url)
