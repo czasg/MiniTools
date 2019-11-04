34b2f569d8980f038015b4d1d789ab45e0d435a7
cza <chenziangsg@163.com>
2019-11-04 23:16:17 +0800

    update base func

--- minitools/__url.py
+++ minitools/__url.py
@@ -4,6 +4,6 @@ __all__ = 'next_page',
 
 
 def next_page(url, rule, step=1, replace=None):
-    url = url.replace(*replace) if replace and re.search(replace[0], url) else None
+    url = url.replace(*replace) if replace and re.search(replace[0], url) else url
     rule = re.sub('(.*)(\(.*)', '(\\1)\\2', rule) if rule.count('(') == 1 else None
     return re.sub(rule, lambda x: f'{x.group(1)}{int(x.group(2)) + step}', url)
--- minitools/scrapy/__pager.py
+++ minitools/scrapy/__pager.py
@@ -1,7 +1,16 @@
-import re
-
 from minitools import next_page
 
-__all__ = 'test',
+__all__ = ('next_page_request',)
 
-test = None
+
+def next_page_request(response, rule, **kwargs):
+    request = response.request
+    method = request.method
+    if method == 'GET':
+        return request.replace(url=next_page(response.url, rule, **kwargs))
+    elif method == 'POST':
+        return request.replace(
+            body=next_page(response.body.decode(), rule, **kwargs).encode(),
+            dont_filter=True)
+    else:
+        raise Exception('This Func just Support `GET`/`POST`!')
