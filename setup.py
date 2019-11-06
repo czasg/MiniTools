9466fb43c13a47b9846754dfb4d2c8a4aefaabb8
cza <czasg0.0>
2019-11-06 08:59:57 +0800

    update the python limit of version

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ from setuptools import setup, find_packages
 2、python setup sdist
 3、twine upload dist/__packages__-__version__.tar.gz
 """
+
 if sys.version_info < (3, 6, 0):
     raise RuntimeError("minitools requires Python 3.6.0+")
 
