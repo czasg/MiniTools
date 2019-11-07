35ef70b46a971b32c92151dc9818f6c4785d4617
cza <chenziangsg@163.com>
2019-11-07 21:29:14 +0800

    update logging module

--- minitools/__logging.py
+++ minitools/__logging.py
@@ -4,14 +4,17 @@ from logging.handlers import TimedRotatingFileHandler
 
 from .__path import current_file_path
 
-__all__ = 'create_logfile',
-
-FORMAT = "[%(asctime)s] %(levelname)s %(module)s[lines-%(lineno)d]: %(message)s"
-logging.basicConfig(format=FORMAT)
+__all__ = ('create_logfile', 'init_logging_format')
 
 
-def create_logfile(logger, filename, pathname, **kwargs):
+def init_logging_format(format=None):
+    format = format or "[%(asctime)s] %(levelname)s %(module)s[lines-%(lineno)d]: %(message)s"
+    logging.basicConfig(format=format)
+
+
+def create_logfile(logger, filename, pathname, format=None, **kwargs):
+    format = format or "[%(asctime)s] %(levelname)s %(module)s[lines-%(lineno)d]: %(message)s"
     trfh = TimedRotatingFileHandler(current_file_path(filename, pathname), **kwargs)
-    formatter = logging.Formatter(FORMAT)
+    formatter = logging.Formatter(format)
     trfh.setFormatter(formatter)
     logger.addHandler(trfh)
