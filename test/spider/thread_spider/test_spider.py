88d131ac7ab592a2e8d77298e88084a6b793f56a
cza <czasg0.0>
2019-11-08 15:05:36 +0800

    add todo problem

--- test/spider/thread_spider/test_spider.py
+++ test/spider/thread_spider/test_spider.py
@@ -2,19 +2,16 @@ from minitools.spider.thread_spider import *
 
 
 class MySpider(Spider):
-    url = 'http://fanyi.youdao.com/'
+    url = 'http://credit.wuhu.gov.cn/whweb/xygs/detailValue?tableName=T_MOULD_PUNISH&coulmnId=f13e789609fb459fa9c5cdaf64d49cbd'
 
     def start_requests(self):
-        for i in range(10):
-            yield Request(self)
+        yield Request(self)
 
     def parse(self, response):
-        print(response.url)
-        yield Request(self, callback=self.parse2)
-
-    def parse2(self, response):
-        print(response.url)
+        print(response.text)
 
 
 if __name__ == '__main__':
     MySpider.run()
+
+# todo, 下载或解析过程中，遇到报错就无法终止程序
