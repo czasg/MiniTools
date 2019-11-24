1eb97f61e06dafb2f4f5147fcdf3b3312e83f190
cza <chenziangsg@163.com>
2019-11-24 23:44:04 +0800

    update

--- minitools/__url.py
+++ minitools/__url.py
@@ -1,8 +1,9 @@
 import re
 
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
+from html.parser import HTMLParser
 
-__all__ = ('next_page', 'HostFilter')
+__all__ = ('next_page', 'HostFilter', 'UrlParser')
 
 
 def next_page(url, rule, step=1, replace=None):
@@ -25,3 +26,20 @@ class HostFilter:
 
     def allow(self, url):
         return bool(self.regex.search(urlparse(url).hostname))
+
+
+class UrlParser(HTMLParser):
+    def __init__(self):
+        super(UrlParser, self).__init__()
+        self.urls = set()
+
+    def handle_starttag(self, tag, attrs):
+        href = dict(attrs).get("href")
+        if href and tag == "a":
+            self.urls.add(href)
+
+    @classmethod
+    def get_links(cls, html, source=None):
+        urlSeeker = cls()
+        urlSeeker.feed(html)
+        return [urljoin(source, url) for url in urlSeeker.urls] if source else urlSeeker.urls
