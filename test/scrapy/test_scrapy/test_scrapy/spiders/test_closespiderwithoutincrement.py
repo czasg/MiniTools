ccc494a1e9ce5d0585a3acbc885118e32ec00cdf
cza <chenziangsg@163.com>
2019-11-30 15:37:14 +0800

    update

---
+++ test/scrapy/test_scrapy/test_scrapy/spiders/test_closespiderwithoutincrement.py
@@ -0,0 +1,21 @@
+from minitools.scrapy import miniSpider
+from minitools.scrapy.extensions import close_spider_without_increment
+from minitools import merge_dict
+
+
+class MySpider(miniSpider):
+    name = "test_spider"
+    start_urls = ["https://www.baidu.com/s?ie=UTF-8&wd=%E7%88%AC%E8%99%AB%E5%A4%A7%E9%87%8F%E8%A3%81%E5%91%98"]
+    custom_settings = merge_dict(
+        close_spider_without_increment,
+        {'CLOSESPIDER_CHECKINTERVAL': '10'}
+    )
+
+    def parse(self, response):
+        import time
+        time.sleep(20)
+        print(response.url)
+
+
+if __name__ == '__main__':
+    MySpider.run(__file__)
