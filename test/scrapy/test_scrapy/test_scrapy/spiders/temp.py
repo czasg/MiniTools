413a35aa37033a0dafaf7206b0d2a31038a4650f
cza <czasg0.0>
2019-12-03 17:45:31 +0800

    update

--- test/scrapy/test_scrapy/test_scrapy/spiders/temp.py
+++ test/scrapy/test_scrapy/test_scrapy/spiders/temp.py
@@ -1,24 +1,20 @@
 # -*- coding: utf-8 -*-
-from minitools.scrapy import miniSpider
+from minitools.scrapy import miniSpider, refresh_encoding
 from scrapy import Request
 
+
 class MySpider(miniSpider):
-    start_urls = ['http://www.nujiang.gov.cn/nj/72913014084337664/20191106/323412.html']
+    start_urls = ['http://www.nujiang.gov.cn/nj/72913014084337664/20191106/323412.html']  # 这个乱码可太嫌人了
 
     def start_requests(self):
         for url in self.start_urls:
-            yield Request(url, encoding='gb2312')
+            yield Request(url)  # ISO-8859-1 gb2312
 
     def parse(self, response):
+        # response = refresh_encoding(response)
+        # print(response.text)
+        response.replace(encoding='gb2312')
         print(response.encoding)
-        print(response.text)
-
-    @classmethod
-    def update_settings(cls, settings):
-        custom_settings = {
-            'FEED_EXPORT_ENCODING': 'gb2312'
-        }
-        settings.setdict(custom_settings, priority='spider')
 
 
 if __name__ == '__main__':
