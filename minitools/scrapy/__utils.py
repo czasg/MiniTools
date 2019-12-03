f89d50e457b13fad7e19ae561b55ef11f5661f19
cza <chenziangsg@163.com>
2019-12-03 22:10:09 +0800

    update

--- minitools/scrapy/__utils.py
+++ minitools/scrapy/__utils.py
@@ -4,7 +4,9 @@ __all__ = "refresh_encoding",
 
 
 # guess the most suitable coding for response by chardet
-def refresh_encoding(response, encoding=None):
+def refresh_encoding(response, body=None, encoding=None):
     response._cached_ubody = None
+    if body:
+        response._body = body
     response._encoding = encoding or chardet.detect(response.body)['encoding']
     return response
