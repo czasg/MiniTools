71dea582da13cd87b8e789b9f4a7cb5eb9d04398
cza <czasg0.0>
2019-12-04 11:12:40 +0800

    update utils

--- minitools/scrapy/__utils.py
+++ minitools/scrapy/__utils.py
@@ -1,6 +1,8 @@
 import chardet
 
-__all__ = "refresh_response",
+from w3lib.encoding import http_content_type_encoding
+
+__all__ = "refresh_response", "guess_coding",
 
 
 # guess the most suitable coding for response by chardet
@@ -10,3 +12,8 @@ def refresh_response(response, body=None, encoding=None):
         response._body = body
     response._encoding = encoding or chardet.detect(response.body)['encoding']
     return response
+
+
+# guess the most suitable coding for body by chardet and w3lib
+def guess_coding(body):
+    return http_content_type_encoding(f'charset={chardet.detect(body)["encoding"]}')
