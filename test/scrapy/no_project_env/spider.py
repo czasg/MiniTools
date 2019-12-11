9e652cb092edfb1d024b72486f4289a5f3a120ad
cza <czasg0.0>
2019-12-11 18:07:02 +0800

    update

--- test/scrapy/no_project_env/spider.py
+++ test/scrapy/no_project_env/spider.py
@@ -2,12 +2,24 @@ from minitools.scrapy import miniSpider
 
 
 class MySpider(miniSpider):
-    start_urls = ["https://www.baidu.com"]
+    start_urls = ["https://www.baidu.com/"]
+    custom_settings = {
+        "CONCURRENT_REQUESTS": 1,
+        "DOWNLOAD_DELAY": 3,
+        "HTTPERROR_ALLOWED_CODES": [521],
+    }
 
     def parse(self, response):
         for i in range(10):
             yield {'cza': 'cza', 'is': 'is', 'sg': '哈哈'}  # Request, BaseItem, dict or None
+        yield response.request.replace(dont_filter=True, callback=self.parse1)
+
+    def parse1(self, response):
+        # self.crawler.engine.close_spider(self, 'just/for/test')
+        print("this is parse1")
 
 
 if __name__ == '__main__':
-    MySpider.run(__file__, save=True)
+    import os
+
+    MySpider.run(__file__, suffix=f"-s JOBDIR={os.path.dirname(__file__)}")
