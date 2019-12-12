dce63539a49cbc876127d45c9888ea61d34be2c5
cza <czasg0.0>
2019-12-12 10:31:09 +0800

    update merge_dicts

--- minitools/__dict.py
+++ minitools/__dict.py
@@ -1,4 +1,4 @@
-__all__ = ('merge_dict',)
+__all__ = ('merge_dict', 'merge_dicts')
 
 
 def merge_dict(dictionary1: dict, dictionary2: dict) -> dict:
@@ -11,4 +11,11 @@ def merge_dict(dictionary1: dict, dictionary2: dict) -> dict:
                 new_dict[key] = merge_dict(dictionary1[key], value)
             elif isinstance(value, list):
                 new_dict[key] = list(set(new_dict[key] + value))
+    return new_dict
+
+
+def merge_dicts(*args):
+    new_dict = {}
+    for arg in args:
+        new_dict = merge_dict(arg, new_dict)
     return new_dict
