108d1c9ec5ba0d3ea1d164c9e28a02e4b5b2a80b
cza <chenziangsg@163.com>
2019-12-14 12:25:37 +0800

    update test code

--- test/picture/ziru/test_ziru.py
+++ test/picture/ziru/test_ziru.py
@@ -1,11 +1,14 @@
-from minitools.picture.ziru import Ziru
+from minitools.picture.ziru import Ziru, ziru
 
 if __name__ == '__main__':
     image = "4983571602.png"
     trainSet = "4983571602.txt"
     Ziru.createTrainingSet(image)
+    zr = ziru()
 
     test_image = '9635804271.png'
     ziru = Ziru(trainSet)
     with open(test_image, 'rb') as f:
-        print(ziru.get_price(f.read()))
+        body = f.read()
+        print(ziru.get_price(body))
+        print(zr.get_price(body))
