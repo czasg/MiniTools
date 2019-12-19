6d4c78e0563c71d9e13b49f8bd14d27faad3e6bb
cza <czasg0.0>
2019-12-19 14:39:10 +0800

    update

--- minitools/__init__.py
+++ minitools/__init__.py
@@ -6,8 +6,10 @@ from .__datetime import *
 from .__dict import *
 from .__email import *
 from .__hashlib import *
+from .__html import *
 from .__logging import *
 from .__path import *
+from .__xpather import *
 from .__url import *
 from .__utils import *
 
@@ -16,7 +18,9 @@ __all__ = (__base64.__all__ +
            __dict.__all__ +
            __email.__all__ +
            __hashlib.__all__ +
+           __html.__all__ +
            __logging.__all__ +
            __path.__all__ +
+           __xpather.__all__ +
            __url.__all__ +
            __utils.__all__)
