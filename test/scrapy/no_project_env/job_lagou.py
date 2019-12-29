cb08b1e3d8c0e05a4e5dd49295bc0ca098e032fc
cza <czasg0.0>
2019-12-29 13:31:16 +0800

    update new spider

--- test/scrapy/no_project_env/job_lagou.py
+++ test/scrapy/no_project_env/job_lagou.py
@@ -1,46 +1,13 @@
 # -*- coding: utf-8 -*-
 import json
 import logging
-from minitools.scrapy import miniSpider, from_xpath
+from minitools import timekiller
+from minitools.db.mongodb import get_mongodb_client
+from minitools.scrapy import miniSpider
 from scrapy import FormRequest, Request
 from scrapy.downloadermiddlewares.cookies import CookiesMiddleware
 
-area = {
-    '11': '北京',
-    '12': '天津',
-    '13': '河北',
-    '14': '山西',
-    '15': '内蒙古',
-    '21': '辽宁',
-    '22': '吉林',
-    '23': '黑龙江',
-    '31': '上海',
-    '32': '江苏',
-    '33': '浙江',
-    '34': '安徽',
-    '35': '福建',
-    '36': '江西',
-    '37': '山东',
-    '41': '河南',
-    '42': '湖北',
-    '43': '湖南',
-    '44': '广东',
-    '45': '广西',
-    '46': '海南',
-    '50': '重庆',
-    '51': '四川',
-    '52': '贵州',
-    '53': '云南',
-    '54': '西藏',
-    '61': '陕西',
-    '62': '甘肃',
-    '63': '青海',
-    '64': '宁夏',
-    '65': '新疆',
-    '71': '台湾',
-    '81': '香港',
-    '82': '澳门',
-}
+mongodb_client = get_mongodb_client()
 
 
 class MySpider(miniSpider):
@@ -53,6 +20,9 @@ class MySpider(miniSpider):
     }
     collect_url = "https://a.lagou.com/collect"
     cookies_middleware = None
+    mongodb_coll = "city_statistics"
+    mongodb_db = "job_lagou"
+    cities_statistics = {}
 
     def check_response(self, response):
         try:
@@ -69,14 +39,10 @@ class MySpider(miniSpider):
         self.cookies_middleware.jars.clear()
 
     def start_requests(self):
-        # yield Request(self.start_urls[0], meta={"dont_merge_cookies": True})
-        self.cities = list(area.values())
+        query_city = self.settings.get("query_city", '武汉|深圳')
+        self.cities = list(filter(lambda x: x.strip(), query_city.split("|")))
+        self.query = self.settings.get("query", 'python')
         yield self.next_city()
-
-    # def parse(self, response):
-    #     all_city = response.xpath('//*[@class="word_list"]//li/a')
-    #     self.cities = [from_xpath(city, './text()') for city in all_city]
-    #     yield self.next_city()
 
     def next_city(self, city=None):
         try:
@@ -85,7 +51,7 @@ class MySpider(miniSpider):
         except:
             self.log("Cities has exhausted", level=logging.WARNING)
         else:
-            return Request(f"https://www.lagou.com/jobs/list_python&city={city}",
+            return Request(f"https://www.lagou.com/jobs/list_{self.query}&city={city}",
                            self.parse_city, meta={"city": city}, dont_filter=True)
 
     def parse_city(self, response):
@@ -93,9 +59,9 @@ class MySpider(miniSpider):
         form = {
             "first": "true",
             "pn": "1",
-            "kd": f"python&city={city}"
+            "kd": f"{self.query}",
         }
-        referer = f"https://www.lagou.com/jobs/list_python&city={city}"
+        referer = f"https://www.lagou.com/jobs/list_{self.query}&city={city}"
         yield FormRequest(f"https://www.lagou.com/jobs/positionAjax.json?px=default"
                           f"&city={city}&needAddtionalResult=false",
                           callback=self.parse_json, formdata=form,
@@ -104,9 +70,20 @@ class MySpider(miniSpider):
     def parse_json(self, response):
         json_data = self.check_response(response)
         if json_data:
-            self.log({response.meta['city']: json_data['content']['positionResult']['totalCount']})
+            self.cities_statistics.setdefault(
+                response.meta['city'], json_data['content']['positionResult']['totalCount'])
             yield self.next_city()
+
+    @staticmethod
+    def close(spider, reason):
+        statistics = mongodb_client[spider.mongodb_db][spider.mongodb_coll]
+        today = int(timekiller.get_today().timestamp())
+        doc = dict(
+            timestamp=today,
+            statistics=json.dumps(spider.cities_statistics, ensure_ascii=False),
+        )
+        statistics.update_one({'timestamp': today}, {"$set": doc}, upsert=True)
 
 
 if __name__ == '__main__':
-    MySpider.run(__file__)
+    MySpider.run(__file__, suffix="-s query=python -s query_city=北京|上海|广州|深圳|杭州|武汉|成都|南京|西安|长沙|天津|东莞")
