65302e96da212ed87b2c03a56c5e1800357e4d09
cza <chenziangsg@163.com>
2020-01-01 12:52:11 +0800

    update

--- minitools/__email.py
+++ minitools/__email.py
@@ -1,8 +1,11 @@
+import logging
 from email.header import Header
 from email.mime.text import MIMEText
 from smtplib import SMTP
 
 __all__ = 'Emailer',
+
+logger = logging.getLogger(__name__)
 
 
 class Emailer:
@@ -22,10 +25,14 @@ class Emailer:
         self.receiver.extend(args)
 
     def send(self, subject, message, receiver=None, html=False):
-        msg = MIMEText(message, 'html' if html else 'plain', 'utf-8')
-        msg['Subject'] = Header(subject, 'utf-8')
-        msg['From'] = self.account
-        msg['To'] = ""
-        recv = receiver or self.receiver
-        assert recv, "You Must Configure Who to receive this email!"
-        self.server.sendmail(self.account, recv, msg.as_string())
+        try:
+            msg = MIMEText(message, 'html' if html else 'plain', 'utf-8')
+            msg['Subject'] = Header(subject, 'utf-8')
+            msg['From'] = self.account
+            msg['To'] = ""
+            recv = receiver or self.receiver
+            assert recv, "You Must Configure Who to receive this email!"
+            self.server.sendmail(self.account, recv, msg.as_string())
+            return True
+        except Exception as e:
+            logger.error(e)
