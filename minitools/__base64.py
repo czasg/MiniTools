9106f855e76bd94c6606585ad99c78b992ae4ef4
cza <chenziangsg@163.com>
2020-01-17 23:33:07 +0800

    update base64

--- minitools/__base64.py
+++ minitools/__base64.py
@@ -6,17 +6,17 @@ __all__ = 'base64img',
 class Base64img:
 
     @staticmethod
-    def bytes2str(body):
-        return base64.b64encode(body).decode()
+    def byte2base64(byte: bytes) -> str:
+        return base64.b64encode(byte).decode()
 
     @staticmethod
-    def str2bytes(string):
+    def base642byte(string: str) -> str:
         return base64.b64decode(string)
 
     @staticmethod
     def fix2prefix(obj):
         if isinstance(obj, bytes):
-            obj = Base64img.bytes2str(obj)
+            obj = Base64img.byte2base64(obj)
         if isinstance(obj, str):
             return f"data:image/png;base64,{obj}"
         raise Exception(f"{obj.__class__} Error, A bytes/str is allowed")
